aa5d02c90b51864a69ab3881db45241ff7dc8514
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-05-03 18:14:23 -0400

    Transponer el tablero para insertar con la columna

--- board/board.go
+++ board/board.go
@@ -1,16 +1,34 @@
 package board
 
+import (
+	"fmt"
+)
+
 type Board struct {
 	Width  int
 	Height int
-	Data   [][]rune
+	Data   [][]string
 }
 
 func New(width int, height int) Board {
 	b := Board{Width: width, Height: height}
-	b.Data = make([][]rune, b.Height)
+	b.Data = make([][]string, b.Height)
 	for i := range b.Data {
-		b.Data[i] = make([]rune, b.Width)
+		b.Data[i] = make([]string, b.Width)
 	}
 	return b
+}
+
+func (b *Board) PrintBoard() {
+	for i := 0; i < b.Width; i++ {
+		for j := 0; j < b.Height; j++ {
+			v := b.Data[j][i]
+			if v == "" {
+				fmt.Print("·")
+			} else {
+				fmt.Print(v)
+			}
+		}
+		fmt.Print("\n")
+	}
 }
